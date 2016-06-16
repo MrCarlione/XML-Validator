f5326ea974c3bce8c7208c2b180e9ac226274d61
GolubevIV <GolubevIV@IGOLUBEVPC>
2016-06-16 15:22:11 +0300

    Add project files.

---
+++ SampleValidator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Xml;
+using System.IO;
+
+namespace SampleValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path to XSD scheme!");
+            string pathToXSD = Console.ReadLine();
+            Console.WriteLine("Enter path to XML file!");
+            string pathToXML = Console.ReadLine();
+            Validate(pathToXSD, pathToXSD);
+        }
+
+        public static void Validate(string pathToXSD, string pathToXML)
+        {
+            XmlTextReader xmlTextReader = new XmlTextReader(pathToXSD);
+            XmlSchema schema = XmlSchema.Read(xmlTextReader, ValidationCallback);
+            FileStream file = new FileStream("new.xml", FileMode.Create, FileAccess.ReadWrite);
+            XmlTextWriter xwriter = new XmlTextWriter(file, new UTF8Encoding());
+            xwriter.Formatting = Formatting.Indented;
+            schema.Write(xwriter);
+
+
+        }
+
+        static void ValidationCallback(object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+                Console.Write("WARNING: ");
+            else if (args.Severity == XmlSeverityType.Error)
+                Console.Write("ERROR: ");
+
+            Console.WriteLine(args.Message);
+        }
+    }
+}
---
+++ Validator/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Validator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseXSDButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectFile(visualizeXSDPath, "XML Schema File\"|*.xsd");
+        }
+
+        private void chooseXMLButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectFile(visualizeXMLPath, "Файл \"XML\"|*.xml");
+        }
+
+        private void validateButton_Click(object sender, RoutedEventArgs e)
+        {
+            ValidateXMLToXSD();
+        }
+
+        private void generateButton_Click(object sender, RoutedEventArgs e)
+        {
+            //var button = (Button)sender;
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            outputTextBox.Clear();
+        }
+
+        void WriteToOutput(object message)
+        {
+            outputTextBox.Text += String.Format("{0} - {1}{2}", DateTime.Now, message, Environment.NewLine);
+        }
+
+        void SelectFile(TextBox tb, string filter)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.AddExtension = true;
+            //dialog.DefaultExt = extension;
+            dialog.Filter = filter;
+
+            var result = dialog.ShowDialog();
+            if (result.Value)
+                tb.Text = dialog.FileName;
+        }
+
+        bool CheckPathExist()
+        {
+            bool result = true;
+            if (String.IsNullOrEmpty(visualizeXSDPath.Text))
+            {
+                WriteToOutput("Не выбран файл .xsd схемы.");
+                result = false;
+            }
+            if (String.IsNullOrEmpty(visualizeXMLPath.Text))
+            {
+                WriteToOutput("Не выбран .xml файл.");
+                result = false;
+            }
+
+            return result;
+        }
+
+
+        void GenerateSimpleXmL()
+        {
+            XmlDocument documentXSD = new XmlDocument();
+            XmlSchemaSet schemaSetXSD = new XmlSchemaSet();
+            try
+            {
+                documentXSD.Load(visualizeXSDPath.Text);
+                schemaSetXSD.Add(null, new XmlNodeReader(documentXSD));
+            }
+            catch (XmlSchemaException xmlException)
+            {
+                WriteToOutput(xmlException.Message);
+            }
+
+            XmlDocument documentXML = new XmlDocument();
+            try
+            {
+                //XmlSampleGenerator s = new XmlSampleGenerator
+                //documentXML.Crea
+            }
+            catch
+            {
+
+            }
+        }
+
+        void ValidateXMLToXSD()
+        {
+            if (CheckPathExist())
+            {
+                progressBar.Value = progressBar.Minimum;
+                XmlDocument documentXSD = new XmlDocument();
+                XmlSchemaSet schemaSetXSD = new XmlSchemaSet();
+                try
+                {
+                    documentXSD.Load(visualizeXSDPath.Text);
+                    schemaSetXSD.Add(null, new XmlNodeReader(documentXSD));
+                }
+                catch (XmlSchemaException xmlException)
+                {
+                    WriteToOutput(xmlException.Message);
+                }
+
+                XmlDocument documentXML = new XmlDocument();
+                try
+                {
+                    documentXML.Load(visualizeXMLPath.Text);
+                    documentXML.Schemas.Add(schemaSetXSD);
+                    documentXML.Validate(new ValidationEventHandler(ValidationHandler));
+                    WriteToOutput("Выбранный файл соответствует .xsd схеме.");
+                    progressBar.Value = progressBar.Maximum;
+                }
+                catch (Exception ex)
+                {
+                    WriteToOutput(ex.Message);
+                }
+            }
+        }
+
+
+        void ValidateXMLToXSDNew()
+        {
+
+        }
+
+        private void ValidationHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+                throw new InvalidOperationException("Предупреждение: " + e.Message, e.Exception);
+            else
+                throw new InvalidOperationException("Ошибка: " + e.Message, e.Exception);
+        }
+    }
+}
